4090d11f54e83b8acee893206eb3d2c3dfa97c81
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-23 21:20:58 -0400

    Sign in

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,27 +1,51 @@
 import React from 'react'
-
-import { View, Text, Button, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button, Input } from 'react-native-elements'
+import Spacer from '../components/Spacer';
 
 const SignInScreen = ({navigation}) => {
 
     return (
         <View>
-            <Text>SignInScreen
-            </Text>
-            <Button 
+
+        <Spacer><Text h4
+        style={style.textStyle}
+        >Sign In for Tracker</Text></Spacer>
+       
+        <Spacer><Input 
+            label="Email Address"
+            placeholder="email"   
+        /></Spacer>
+
+        <Spacer><Input 
+            label="Password"
+            placeholder="password"
+        /></Spacer>
+
+        <Spacer><Button 
             title="Go to Sign Up" 
             onPress={() => navigation.navigate('SignUp')} 
-            />
+        /></Spacer>
+
+        <Spacer>
+            <Button 
+            title="Sign In" 
+            onPress={() => navigation.navigate('mainFlow')} 
+        /></Spacer>
         </View>
     )
 }
 
 const style = StyleSheet.create({
     viewStyle: {
-
+        flex:1,
+       // justifyContent:"space-evenly",
+        // alignContent:"center",
+        // margin:20
     },
     textStyle: {
-        
+        textAlign:"center"
     }
 })
 
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,32 +1,53 @@
 import React from 'react'
 
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button, Input } from 'react-native-elements'
+
+import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({navigation}) => {
 
     return (
-        <View style={style.viewStyle}>
-            <Text>SignUpScreen
-            </Text>
-            <Button 
-                title="Go to Sign In" 
+        <View >
+        
+            <Spacer><Text h4
+            style={style.textStyle}
+            >Sign Up for Tracker</Text></Spacer>
+           
+            <Spacer><Input 
+                label="Email Address"
+                placeholder="email"   
+            /></Spacer>
+
+            <Spacer><Input 
+                label="Password"
+                placeholder="password"
+            /></Spacer>
+
+            <Spacer><Button 
+                title="Sign Up" 
                 onPress={() => navigation.navigate('SignIn')} 
-            />
-            <Button 
-                title="Go to main flow In" 
-                onPress={() => navigation.navigate('mainFlow')} 
-        />
+            /></Spacer>
+
         </View>
     )
 }
 
+SignUpScreen.navigationOptions = () => {
+    return {
+      header: () => false,
+    };
+  };
+
 const style = StyleSheet.create({
     viewStyle: {
         flex:1,
-        margin:20
+       // justifyContent:"space-evenly",
+        // alignContent:"center",
+        // margin:20
     },
     textStyle: {
-        
+        textAlign:"center"
     }
 })
 
