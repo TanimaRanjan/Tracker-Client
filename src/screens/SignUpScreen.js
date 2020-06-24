62cdc4cb301a0b32bd16b1184d40765e521b4317
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-23 20:41:11 -0400

    Routing from signup to sign in

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -5,20 +5,20 @@ import { View, Text, Button, StyleSheet } from 'react-native'
 const SignUpScreen = ({navigation}) => {
 
     return (
-        <>
+        <View style={style.viewStyle}>
             <Text>SignUpScreen
             </Text>
             <Button 
             title="Go to signIn" 
             onPress={() => navigation.navigate('SignIn')} 
             />
-        </>
+        </View>
     )
 }
 
 const style = StyleSheet.create({
     viewStyle: {
-
+        margin:20
     },
     textStyle: {
         
