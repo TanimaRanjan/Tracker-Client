3b3e3ef2cefb9c4a82044c19a54d1d3178b21c8c
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-26 13:17:12 -0400

    Mocking location for testing

--- src/_mockLocation/_mockLocation.js
+++ src/_mockLocation/_mockLocation.js
@@ -1,31 +1,29 @@
-import * as Location from 'expo-location'
-
-const tenMetersWithDegrees = 0.0001
+import * as Location from 'expo-location';
+const tenMetersWithDegrees = 0.0001;
 
 const MY_LAT =40.7246703
 const MY_LNG=-73.9511656
 
 const getLocation = increment => {
-    return {
-        timestamp:10000000,
-        coords: {
-            speed:0,
-            heading:0,
-            accuracy:5,
-            altitudeAccuracy:5,
-            altitude:5,
-            longitude:40.7246703 + increment * tenMetersWithDegrees,
-            latitude:-73.9511656 + increment * tenMetersWithDegrees,
-        }
+  return {
+    timestamp: 10000000,
+    coords: {
+      speed: 0,
+      heading: 0,
+      accuracy: 5,
+      altitudeAccuracy: 5,
+      altitude: 5,
+      longitude: MY_LNG + increment * tenMetersWithDegrees,
+      latitude: MY_LAT + increment * tenMetersWithDegrees
     }
-}
+  };
+};
 
-
-let counter = 0
+let counter = 0;
 setInterval(() => {
-    Location.EventEmitter.emit('Expo.locationChanges', {
-        watchId:Location._getCurrentWatchId(),
-        location:getLocation(counter)
-    })
-    counter++
-}, 1000)
+  Location.EventEmitter.emit('Expo.locationChanged', {
+    watchId: Location._getCurrentWatchId(),
+    location: getLocation(counter)
+  });
+  counter++;
+}, 1000);
