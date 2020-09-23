c371691497737cd6fcd9af9743c8e014d9de6ebc
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-23 10:34:23 -0400

    Payload and type destructured from action

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -1,16 +1,17 @@
 import createDataContext from './createDataContext'
 
 const locationReducer = (state, action) => {
-    
-    switch(action.type) {
+
+    const {type, payload } = action
+    switch(type) {
         case 'START_RECORDING':
             return {...state, recording:true}
         case 'STOP_RECORDING':
             return {...state, recording:false}
         case 'ADD_CURRENT_LOCATION':
-            return {...state , currentLocation:action.payload}
+            return {...state , currentLocation:payload}
         case 'ADD_LOCATION':
-            return {...state , locations: [...state.locations, action.payload]}
+            return {...state , locations: [...state.locations, payload]}
         case 'CHANGE_NAME':
             return {...state, name:payload}
         default:
