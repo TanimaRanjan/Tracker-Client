3990c2c2ef56c7dd3873eb1893541d6360f805b0
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-23 11:18:29 -0400

    Adding save button and saving on click after few locations are loaded

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,40 +1,51 @@
-import React, {useContext } from 'react'
-import { Input, Button } from 'react-native-elements'
-import {Context as LocationContext } from '../context/LocationContext'
+import React, {useContext} from 'react'
+import {Input, Button} from 'react-native-elements'
+import {Context as LocationContext} from '../context/LocationContext'
+import useSaveTrack  from "../hooks/useSaveTrack";
 import Spacer from './Spacer'
 
 const TrackForm = () => {
 
-    const {state:{name, recording, locations }, 
-                startRecording, 
-                stopRecording, 
-                changeName}  = useContext(LocationContext)
+    const {
+        state: {name, recording, locations},
+        startRecording,
+        stopRecording,
+        changeName
+    } = useContext(LocationContext)
 
+    const [saveTrack ] = useSaveTrack();
     return (
         <>
-        <Spacer>
-        <Input 
-            placeholder="Enter name"
-            value={name}
-            onChangeText={changeName}
-        />
-        {recording ? 
-            <Button 
-            title="Stop Recording"
-            onPress={stopRecording}
-        />
-            :
-            <Button 
-            title="Start Recording"
-            onPress={startRecording}
-        />
-        }
-        
-        </Spacer>
+            <Spacer>
+                <Input
+                    placeholder="Enter name"
+                    value={name}
+                    onChangeText={changeName}
+                />
+                <Spacer>
+                    {recording ?
+                        <Button
+                            title="Stop Recording"
+                            onPress={stopRecording}
+                        />
+                        :
+                        <Button
+                            title="Start Recording"
+                            onPress={startRecording}
+                        />
+                    }
+                </Spacer>
+                <Spacer>
+                    {!recording && locations.length ?
+                        <Button title="Save Recording" onPress={saveTrack}/>
+                        :
+                        null
+                    }
+                </Spacer>
+            </Spacer>
 
         </>
     )
 }
-
 
 export default TrackForm
