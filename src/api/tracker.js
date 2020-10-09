b21c0094f8f75934fc7b055ad482bbf07876107e
TanimaRanjan <tanimaranjan@gmail.com>
2020-10-08 22:02:32 -0400

    Instance iterpretor

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,28 @@
 import axios from 'axios'
+import { AsyncStorage} from "react-native";
 
 // Server start 
 //ngrok http 3000
 
-export default axios.create({
-    baseURL:'http://3ab7c898ac03.ngrok.io'
+const instance =  axios.create({
+    baseURL:' http://27fd39f3fdc8.ngrok.io'
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        try {
+            const token = await AsyncStorage.getItem(('token'))
+            if(token) {
+                config.headers.Authorization = `Bearer ${token}`
+            }
+        } catch (error) {
+            console.log('ERROR !!!!!! ', error)
+        }
+
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
